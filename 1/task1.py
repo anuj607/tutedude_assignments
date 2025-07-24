6d15fe305f9990d1f12db090300edebf47a3097f
anuj607 <anujkumar607@gmail.com>
2025-07-24 17:05:10 +0530

    Assignment task1 and task2

---
+++ 1/task1.py
@@ -0,0 +1,15 @@
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+num1 = int(num1)
+num2 = int(num2)
+
+addition = num1 + num2
+subtract = num1 - num2
+multiply = num1 * num2
+division = num1 / num2
+
+print('Addition', addition)
+print('Subtract', num2)
+print('Multiply', num2)
+print('Division', division)
---
+++ 1/task2.py
@@ -0,0 +1,4 @@
+firstname = input('Enter your first name: ').strip().title()
+lastname = input('Enter your last name: ').strip().title()
+
+print('Hello',firstname +' '+ lastname +'!' + ' Welcome to the Python program' )
