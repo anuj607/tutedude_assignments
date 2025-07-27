57ef011156c0b2091c48febcfb05cf156e1b8817
anuj607 <anujkumar607@gmail.com>
2025-07-27 19:38:34 +0530

    assignment 3 factorial and Math fun

---
+++ 3/task2.py
@@ -0,0 +1,14 @@
+import math
+
+number = int(input('Enter a number '))
+
+sqaureRoot = math.sqrt(number)
+
+mathLogRoot = math.log(number)
+
+mathSin = math.sin(number)
+
+
+print('Square root ',sqaureRoot)
+print('Logarithm ',mathLogRoot)
+print('Sine ',mathSin)
